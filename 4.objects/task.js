f211003ba5ea41e6efa0fc5e652afd4bdf4b0a30
sokovv <88129553+sokovv@users.noreply.github.com>
2021-10-26 13:12:25 +0300

    Homework4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,38 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (mark1, mark2, mark3, ...rest) {
+  this.marks = [mark1, mark2, mark3, ...rest];
+}
+
+Student.prototype.getAverage = function () {
+  let sum;
+  sum = 0;
+
+  for (let i = 0; i < this.marks.length; i += 1) {
+    sum += this.marks[i];
+  }
+  return +(sum / this.marks.length);
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
+}
+

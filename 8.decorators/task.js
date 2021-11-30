460f4207bca6b33012c346dea24e6cdc53865155
Valera Sokolov <komproekt42@gmail.com>
2021-11-30 21:19:02 +0300

    ДЗ Декораторы

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,49 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = {};
+  function wrapper(...args) {
+    const key = args.join(',')
+    if (key in cache) {
+      console.log("Из кэша: " + cache[key]);
+      return "Из кэша: " + cache[key];
+    } else {
+      const result = func(...args)
+      cache[key] = result
+      if (Object.keys(cache).length > 5) {
+        delete cache[Object.keys(cache)[0]];
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    }
+  }
+  return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let timer;
+  function wrapper(...args) {
+    if (timer) {
+      clearTimeout(timer)
+      timer = setTimeout(() => func(...args), ms)
+    } else {
+      timer = setTimeout(func)
+    }
+  }
+  return wrapper;
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, ms) {
+  let timer;
+  function wrapper(...args) {
+    wrapper.count.push(args);
+    if (timer) {
+      clearTimeout(timer)
+      timer = setTimeout(() => func(...args), ms)
+    } else {
+      timer = setTimeout(func)
+    }
+  }
+  wrapper.count = [];
+  console.log(wrapper.count)
+  return wrapper;
 }
